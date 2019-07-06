2fd6afd768b067fef70a94fb424220622906ce77
Rurick Maqueo Poisot <skintigthvader@gmail.com>
2019-07-06 17:58:04 -0500

    added methjods to manage wishLists

--- backend/usecases/user.js
+++ backend/usecases/user.js
@@ -12,10 +12,34 @@ const create = ({ name, lastname, email, address, role }) => {
 
 const getById = ( id ) => User.findById(id)
 
-const update = (id, { name, lastname, email, address, role }) => {
-  const userUpdated = User.findByIdAndUpdate(id, { name, lastname, email, address, role })
+const update = (userId, { name, lastname, email, address, role }) => {
+  const userUpdated = User.findByIdAndUpdate(userId, { name, lastname, email, address, role })
 }
 
-const createAWhishList = () => {
-  
+const updatePersonality = (userId, personality) => {
+  const updatedUser = User.findByIdAndUpdate(userId, personality)
+}
+
+const addWhishList = ( id, { name, items, public }) => {
+  const { wishLists } = User.findById(id)
+  const newWishList = { name, items, public }
+  wishLists.push(newWishList)
+  const updatedWishList = User.findByIdAndUpdate(id, { wishLists: newWishList })
+}
+
+const deleteWishlist = (userId, index) => {
+  let { wishLists } = User.findById(userId)
+  wishLists  = wishLists.splice(index, 1)
+  const updatedWishList = User.findByIdAndUpdate(userId, { wishLists: updatedWishList })
+}
+
+const toggleWishListPrivacy = (userId, wishListIndex) => {
+  const { wishLists } = User.findById(userId)
+  wishLists[wishListIndex].public = !(wishLists[wishListIndex].public)
+}
+
+
+//ToDo: 
+const removeItemFromWishList = (userId, wishListIndex, itenIndex) => {
+
 }
