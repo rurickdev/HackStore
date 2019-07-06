03fa50277677d49696b39096cfff286722f9c23d
Rurick M. Poisot <rurick@leco.mx>
2019-07-06 15:36:06 -0500

    Added all the use cases for category model

---
+++ backend/usecases/category.js
@@ -0,0 +1,17 @@
+const Category = require('../models/category')
+
+const create = ( name ) => {
+  const nameNoraliced = name.
+}
+
+const getAll = () => Category.find()
+
+const getNameById = ( id ) => Category.findById( id )
+
+const deleteById = ( id ) => Category.remove({ id })
+
+module.exports = {
+  getAll,
+  getNameById,
+  deleteById
+}
