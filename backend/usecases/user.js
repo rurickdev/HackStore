05a65c41b450a2a4ba83e0f0f1de1204c642f262
Rurick Maqueo Poisot <skintigthvader@gmail.com>
2019-07-06 15:49:40 -0500

    created Create method

--- backend/usecases/user.js
+++ backend/usecases/user.js
@@ -1,1 +1,11 @@
 const User = require('../models/user')
+
+const create = ({ name, lastname, email, address, role }) => {
+  const user = User.create({
+    name,
+    lastname,
+    email,
+    address,
+    role
+  })
+}
