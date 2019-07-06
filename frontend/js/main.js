4600844776d4375e41dead934a75368b32f2bb0c
Israel2709 <israel.salinas.m@gmail.com>
2019-07-06 16:59:48 -0500

    [FEAT] Chatbot front

---
+++ frontend/js/main.js
@@ -0,0 +1,38 @@
+let chatbotJson = [
+	{
+		sender:"chatbot",
+		message:"¡Hola! ¿Cómo puedo ayudarte?"
+	}
+]
+
+const sendQuestion = ()=>{
+	console.log("question")
+	let message = $("#user-question").val();
+	let questionObject = {sender:"user",message};
+	chatbotJson.push(questionObject);
+	console.log(message)
+	addMessage(message);
+}
+
+const addMessage = (message)=>{
+	$(".chatbot-body").append(`
+		<div class="user-dialog">
+			<span>${message}</span>
+			<i class="fas fa-robot"></i>
+		</div>
+	`)
+}
+
+$("#send-chatbot-message").on("click",() => {
+	sendQuestion();
+})
+
+$(".activate-chatbot, .close-chatbot").on("click",()=>{
+	console.log("aC")
+	toggleChatbot();
+})
+
+const toggleChatbot = ()=>{
+	$(".chatbot-wrapper").toggleClass("shown");
+	$(".black-overlay").fadeToggle("fast");
+}
