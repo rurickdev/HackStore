8c2be8f57cb5bdea1871d0676c82fefcbbd48535
Rurick Maqueo Poisot <skintigthvader@gmail.com>
2019-07-06 22:20:12 -0500

    added middleware adding_interaction

---
+++ backend/middlewares/adding_interaccion.js
@@ -0,0 +1,41 @@
+const Item = require('../usecases/item')
+const Interaction = require('../usecases/interaction')
+
+const addingInteraction = async (req, res, next) => {
+  const { userId, interactionType, itemId } = req.body
+  let statement = ''
+  let keyWordsString = ''
+  const { name, keyWords } = await Item.getItemById(itemId)
+
+  keyWords.forEach( keyWord => {
+    keyWords = keyWords + ', '+ keyWord
+  });
+
+  switch (interactionType) {
+    case 'visit':
+      statement = `Me interesa ${name} con las siguientes caracteristicas ${keyWordsString}`
+      break
+    case 'buy':
+      statement = `Compré ${name} con las siguientes caracteristicas ${keyWordsString}`
+      break
+    case 'like':
+      statement = `Me gusta ${name} con las siguientes caracteristicas ${keyWordsString}`
+      break
+    case 'dislike':
+      statement = `Ya no me gusta ${name} con las siguientes caracteristicas ${keyWordsString}`
+      break
+    case 'notInterested':
+      statement = `No me interesa ${name} con las siguientes caracteristicas ${keyWordsString}`
+      break
+    default:
+      //code here
+  }
+
+  const interaction = await Interaction.create({ userId, interaccion: statement })
+
+  next()
+}
+
+module.exports = {
+  addingInteraction
+}
